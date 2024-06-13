b5a79121966e3d888520a0f5fe926990621865b1
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-06-13 22:35:36 +0700

    Add theme colors and default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray": "#7f7f7f",
+        "light-gray": "#e8e5e480",
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
