592810b735f2a76418401b7ac75d0d943308a0a8
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-06-14 14:18:54 +0700

    Fix generate navbarLink no key

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,8 +25,9 @@ function Navbar() {
   return (
     <div className="fixed top-0 left-0 w-full flex justify-center h-[88px] md:h-[108px] px-2 py-5 md:px-[40px] md:py-[30px] bg-transparent">
       <div className="flex gap-2 w-full max-w-[400px] h-full bg-light-gray rounded-[50px] p-1 overflow-hidden backdrop-blur-lg">
-        {links.map((link) => (
+        {links.map((link, index) => (
           <NavbarLink
+            key={index}
             name={link.name}
             href={link.href}
             isSelected={activeLink === link.name}
